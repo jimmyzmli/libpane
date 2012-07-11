36d6f7d9451cfac27218be38c75d3bfa413afb6a
Jimmy Li <contact@jimmyzmli.ca>
2012-07-11 00:46:36 -0400

    Fixed panel incompatibility

--- panel.c
+++ panel.c
@@ -1,6 +1,7 @@
 #include "cli.h"
 
-static const char* menu_highlight = "\x1B[35m";
+static const char* menu_highlight = "\x1B[35m\x1B[41m";
+static const char* menu_highlightrst = "\x1B[39m\x1B[49m";
 
 bool panel_console_evt(PANEL *p, int evt){
   int k, b;
@@ -8,7 +9,7 @@ bool panel_console_evt(PANEL *p, int evt){
   bool (*cb)(PANEL*,char*);
   cb = (void*)p->attr[PANEL_MGR_STORE];
   b = p->attr[PANEL_MGR_STORE+1];
-  if( evt == PANEL_CONSOLE_FOCUS ){
+  if( evt == PANEL_FOCUS ){
     panel_focus(p);
     panel_write(p,">>");
     panel_focus(p);
@@ -32,22 +33,22 @@ bool panel_console(PANEL *p, char b, bool (*cb)(PANEL*,char*)){
   p->attr[PANEL_MGR_STORE+1] = b;
 }
 
-bool panel_menu_evt(PANEL *p, int evt){
+bool panel_list_evt(PANEL *p, int evt){
   int top, len, x, y, emode, k;
   bool (*cb)(PANEL*,int);
   top = p->attr[PANEL_MGR_STORE]; len = p->attr[PANEL_MGR_STORE+1];
   cb = (void*)p->attr[PANEL_MGR_STORE+2];
   panel_getcursorpos(p,&x,&y);
   
-  if( evt == PANEL_MENU_UP && y > top ){
-    panel_writeattr(p,"\x1B[39m",20);
+  if( evt == PANEL_UP && y > top ){
+    panel_writeattr(p,menu_highlightrst,20);
     panel_setcursorpos(p,x,y-1);
     panel_writeattr(p,menu_highlight,20);
-  }else if( evt == PANEL_MENU_DOWN && y < top+len-1 ){
-    panel_writeattr(p,"\x1B[39m",20);
+  }else if( evt == PANEL_DOWN && y < top+len-1 ){
+    panel_writeattr(p,menu_highlightrst,20);
     panel_setcursorpos(p,x,y+1);
     panel_writeattr(p,menu_highlight,20);
-  }else if( evt == PANEL_MENU_SELECT ){
+  }else if( evt == PANEL_SELECT ){
     if( cb != 0 )
       if(!cb(p,y-top)) return false;
   }
@@ -55,77 +56,94 @@ bool panel_menu_evt(PANEL *p, int evt){
   return true;
 }
 
-bool panel_menu(PANEL *p, char** items, int len, int def, bool (*cb)(PANEL*,int)){
-  int i, k, x, y, top, bottom;
+bool panel_list(PANEL *p, char* (*items)(int), int def, bool (*cb)(PANEL*,int)){
+  int i, len, k, x, y, top, bottom;
+  char* txt;
   bool emode;
   panel_write(p,"\n");
   panel_getcursorpos(p,&x,&top);
   p->attr[PANEL_MGR_STORE] = top;
-  p->attr[PANEL_MGR_STORE+1] = len;
   p->attr[PANEL_MGR_STORE+2] = (int)cb;
-  for(i=0;i<len;i++){
-    panel_write(p,items[i]);
+  p->attr[PANEL_MGR_STORE+3] = (int)items;
+  for(i=0;(txt=items(i))!=0;i++){
+    panel_write(p,txt);
     panel_write(p,"\n");
   }
+  p->attr[PANEL_MGR_STORE+1] = i;
   panel_setcursorpos(p,x,top+def);
   panel_writeattr(p,menu_highlight,20);
   panel_display(p);
   return true;
 }
 
-int panel_grid_findcol(int* widths,int len,int s,int x){
+static int panel_grid_findcol(int* widths,int len,int s,int x){
   int i,t;
   t = s;
   for(i=0;i<len;i++){
     t += widths[i];
-    if( x < t ) break;
+    if( x < t ) return i;
   }
-  return i;
+  return len-1;
 }
 
-int panel_grid_findcoloffset(int *widths,int len,int s){
+static int panel_grid_findcoloffset(int *widths,int len,int s){
   int j,t;
   t = s;
-  for(j=0;j<len;j++) t += widths[j];
-  return t;
+  for(j=0;j<len;j++) t += widths[j]+1;
+  return t+1;
 }
 
 bool panel_grid_evt(PANEL *p, int evt){
   bool (*cb)(PANEL*,int,int);
-  int topX, topY, x, y, *maxw, col,ofst;
-  topX = p->attr[PANEL_MGR_STORE+3];
-  topY = p->attr[PANEL_MGR_STORE+4];
-  maxw = (int*)p->attr[PANEL_MGR_STORE+2];
-  cb = (void*)p->attr[PANEL_MGR_STORE+1];
+  int topX, topY, x, y, w, h, *maxw, col,ofst;
+  /* Deserialize and restore data */
+  topX = p->attr[PANEL_MGR_STORE+3]; topY = p->attr[PANEL_MGR_STORE+4];
+  maxw = (int*)p->attr[PANEL_MGR_STORE+2]; cb = (void*)p->attr[PANEL_MGR_STORE+1];
+  w = p->attr[PANEL_MGR_STORE+5]; h = p->attr[PANEL_MGR_STORE+6];
   panel_getcursorpos(p,&x,&y);
-  col = panel_grid_findcol(maxw,7,topX,x);
+  col = panel_grid_findcol(maxw,w,topX,x);
   ofst = panel_grid_findcoloffset(maxw,col,topX);
-  if( evt == PANEL_GRID_UP ){
-    panel_setcursorpos(p,ofst,y-1);
+  if( evt == PANEL_UP ){
+    panel_writeattr(p,menu_highlightrst,maxw[col]);
+    panel_setcursorpos(p,ofst,y-2);
     panel_writeattr(p,menu_highlight,maxw[col]);
-  }else if( evt == PANEL_GRID_DOWN ){
-    panel_setcursorpos(p,ofst,y+1);
+    panel_focus(p);
+  }else if( evt == PANEL_DOWN ){
+    panel_writeattr(p,menu_highlightrst,maxw[col]);
+    panel_setcursorpos(p,ofst,y+2);
     panel_writeattr(p,menu_highlight,maxw[col]);
-  }else if( evt == PANEL_GRID_LEFT ){
-    panel_setcursorpos(p,panel_grid_findcoloffset(maxw,col-1,topX),y);
+    panel_focus(p);
+  }else if( evt == PANEL_LEFT && col > 0 ){
+    ofst = panel_grid_findcoloffset(maxw,col-1,topX);
+    panel_writeattr(p,menu_highlightrst,maxw[col]);
+    panel_setcursorpos(p,ofst,y);
     panel_writeattr(p,menu_highlight,maxw[col-1]);
-  }else if( evt == PANEL_GRID_RIGHT ){
+    panel_focus(p);
+  }else if( evt == PANEL_RIGHT && col < w-1 && col >= 0 ){
+    ofst = panel_grid_findcoloffset(maxw,col+1,topX);
+    panel_writeattr(p,menu_highlightrst,maxw[col]);
     panel_setcursorpos(p,panel_grid_findcoloffset(maxw,col+1,topX),y);
     panel_writeattr(p,menu_highlight,maxw[col+1]);
-  }else if( evt == PANEL_GRID_SELECT ){
+    panel_focus(p);
+  }else if( evt == PANEL_SELECT ){
     if( !cb(p,col,y) ) return false;
   }
   return true;
 }
+
 bool panel_grid(PANEL *p, char*(*item)(int,int), int* maxw, int w, int h, bool (*cb)(PANEL*,int,int)){
-  int i, j, width;
+  int i, j, width, topX, topY;
   char linesep[1024], buf[1024], *t;
   p->attr[PANEL_MGR_STORE] = (int)item;
   p->attr[PANEL_MGR_STORE+1] = (int)cb;
   p->attr[PANEL_MGR_STORE+2] = (int)maxw;
-  panel_getcursorpos(p,&(p->attr[PANEL_MGR_STORE+3]),&(p->attr[PANEL_MGR_STORE+4]));
+  panel_getcursorpos(p,&topX,&topY);
+  p->attr[PANEL_MGR_STORE+3] = topX; p->attr[PANEL_MGR_STORE+4] = topY;
+  p->attr[PANEL_MGR_STORE+5] = w;
+  p->attr[PANEL_MGR_STORE+6] = h;
   width = 0;
   for(i=0;i<w;i++) width += maxw[i];
+  width += w+1;
   memset(linesep,'-',width); linesep[width] = 0;
   t = 0;
   for(i=0;i<h;i++){
@@ -134,7 +152,7 @@ bool panel_grid(PANEL *p, char*(*item)(int,int), int* maxw, int w, int h, bool (
     for(j=0;j<w;j++){
       t = item(j,i);
       if( t != 0 ){
-	sprintf(buf,"|%s",t);
+	sprintf(buf,"|%-*.*s",maxw[j],maxw[j],t);
 	panel_write(p,buf);
       }else{
 	break;
@@ -143,4 +161,6 @@ bool panel_grid(PANEL *p, char*(*item)(int,int), int* maxw, int w, int h, bool (
     if( t==0 ) break;
     panel_write(p,"|\n");
   }
+  panel_setcursorpos(p,topX+1,topY+1);
+  panel_writeattr(p,menu_highlight,maxw[0]);
 }
